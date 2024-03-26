c59c3d21c1f50a208e035de4a99bce25066f197e
entacleo1 <entacleoenito@gmail.com>
2024-03-26 14:28:53 +0800

    myfirstcommit

---
+++ src/testapp2024/Testapp2024.java
@@ -0,0 +1,17 @@
+
+package testapp2024;
+
+/**
+ *
+ * @author SCC1
+ */
+public class Testapp2024 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
